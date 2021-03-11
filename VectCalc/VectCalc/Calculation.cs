922bcb3c245b624724d1c8b7c9fa291162d51b07
CharlieW1799 <78801983+CharlieW1799@users.noreply.github.com>
2021-03-12 00:49:16 +0300

    Add files via upload

---
+++ VectCalc/VectCalc/Calculation.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectCalc
+{
+    public class Calculation
+    {
+        static public double GetResult(string input)
+        {
+            string output = ToRPN(input);
+            double result = Calculate(output);
+            return result;
+        }
+
+        static private string ToRPN(string input)
+        {
+            string rpn = string.Empty;
+            Stack<char> operatorsStack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsDelimeter(input[i]))
+                    continue;
+
+                if (Char.IsDigit(input[i]))
+                {
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        rpn += input[i];
+                        i++;
+
+                        if (i == input.Length) break;
+                    }
+
+                    rpn += " ";
+                    i--;
+                }
+
+                if (IsOperator(input[i]))
+                {
+                    if (input[i] == '(')
+                        operatorsStack.Push(input[i]);
+
+                    else if (input[i] == ')')
+                    {
+                        char s = operatorsStack.Pop();
+
+                        while (s != '(')
+                        {
+                            rpn += s.ToString() + ' ';
+                            s = operatorsStack.Pop();
+                        }
+                    }
+
+                    else
+                    {
+                        if (operatorsStack.Count > 0)
+                            if (GetPriority(input[i]) <= GetPriority(operatorsStack.Peek()))
+                                rpn += operatorsStack.Pop().ToString() + " ";
+
+                        operatorsStack.Push(char.Parse(input[i].ToString()));
+
+                    }
+                }
+            }
+
+            while (operatorsStack.Count > 0)
+                rpn += operatorsStack.Pop() + " ";
+
+            return rpn;
+        }
+
+        static private double Calculate(string input)
+        {
+            double result = 0;
+            Stack<double> temp = new Stack<double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsDigit(input[i]))
+                {
+                    string resStr = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        resStr += input[i];
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(resStr));
+                    i--;
+                }
+
+                else if (IsOperator(input[i]))
+                {
+                    double first = temp.Pop();
+                    double second = temp.Pop();
+
+                    switch (input[i])
+                    {
+                        case '+': result = second + first; 
+                            break;
+                        case '-': result = second - first; 
+                            break;
+                        case '*': result = second * first; 
+                            break;
+                        case '/': result = second / first; 
+                            break;
+                        case '^': result = double.Parse(Math.Pow(double.Parse(second.ToString()), double.Parse(first.ToString())).ToString());
+                            break;
+                    }
+                    temp.Push(result);
+                }
+            }
+            return temp.Peek();
+        }
+
+        static private bool IsDelimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*^()".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+
+        static private byte GetPriority(char c)
+        {
+            switch (c)
+            {
+                case '(': return 0;
+                case ')': return 0;
+                case '+': return 1;
+                case '-': return 1;
+                case '*': return 2;
+                case '/': return 2;
+                case '^': return 3;
+                default: return 10;
+            }
+        }
+    }
+}
---
+++ VectCalc/VectCalc/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace VectCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbRes = new System.Windows.Forms.TextBox();
+            this.btnComp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter an expression:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Result:";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbInput.Location = new System.Drawing.Point(12, 69);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(281, 34);
+            this.tbInput.TabIndex = 2;
+            // 
+            // tbRes
+            // 
+            this.tbRes.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbRes.Location = new System.Drawing.Point(12, 169);
+            this.tbRes.Name = "tbRes";
+            this.tbRes.ReadOnly = true;
+            this.tbRes.Size = new System.Drawing.Size(281, 34);
+            this.tbRes.TabIndex = 3;
+            // 
+            // btnComp
+            // 
+            this.btnComp.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnComp.Location = new System.Drawing.Point(80, 226);
+            this.btnComp.Name = "btnComp";
+            this.btnComp.Size = new System.Drawing.Size(146, 38);
+            this.btnComp.TabIndex = 4;
+            this.btnComp.Text = "Compute plz";
+            this.btnComp.UseVisualStyleBackColor = true;
+            this.btnComp.Click += new System.EventHandler(this.btnComp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 293);
+            this.Controls.Add(this.btnComp);
+            this.Controls.Add(this.tbRes);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vectorian Calculyactor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbRes;
+        private System.Windows.Forms.Button btnComp;
+    }
+}
+
