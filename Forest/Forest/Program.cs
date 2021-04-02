ede07b84e9f81b94cb6b0d3294566be1f4deddb2
CharlieW1799 <78801983+CharlieW1799@users.noreply.github.com>
2021-04-02 13:54:33 +0300

    Add files via upload

---
+++ Forest/Forest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Forest
+{
+    public class Node
+    {
+        public int data;
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        public Node(int x)
+        {
+            this.data = x;
+        }
+    }
+
+    class Stree
+    {
+        Node root;
+        public void Insert(int x)
+        {
+            if (root == null)
+                root = new Node(x);
+            else
+                Insert(x, root);
+        }
+        private void Insert(int x, Node p)
+        {
+            if (x < p.data)
+            {
+                if (p.left == null)
+                    p.left = new Node(x);
+                else
+                    Insert(x, p.left);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
